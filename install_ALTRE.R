2347309504b1a1837acbd8c5b1de5fe0539166ac
rbonneville <russell.bonneville@osumc.edu>
2016-09-14 17:59:49 -0400

    Move windows github hack after bioconductor

--- install_ALTRE.R
+++ install_ALTRE.R
@@ -1,9 +1,6 @@
 setRepositories(ind=1:3)
 install.packages("devtools")
-if (.Platform$OS.type == "windows")
-{
-	devtools::install_github("hadley/devtools")
-}
+library(devtools)
 r.major.version <- as.numeric(R.Version()$major)
 r.minor.version <- as.numeric(R.Version()$minor)
 if (length(grep("darwin", R.Version()$os)) > 0 && (r.major.version <= 2 || (r.major.version == 3 && r.minor.version < 3)))
@@ -14,10 +11,13 @@ if (length(grep("darwin", R.Version()$os)) > 0 && (r.major.version <= 2 || (r.ma
 	source("https://bioconductor.org/biocLite.R")
 }
 biocLite(c("org.Hs.eg.db","EnsDb.Hsapiens.v75","GO.db"))
-devtools::install_github("mathelab/ALTRE",ref="webgraphs",dependencies=TRUE)
+if (.Platform$OS.type == "windows")
+{
+	install_github("hadley/devtools")
+}
+install_github("mathelab/ALTRE",ref="webgraphs")
 if (.Platform$OS.type == "windows")
 {
 	# don't permamently replace the user's devtools
 	install.packages("devtools")
 }
-
